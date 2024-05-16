87f723956d605a867624839195f340a8a2f2935e
Moskaclvhuk VR <mmvvrr1@gmail.com>
2024-05-16 09:55:53 +0900

    Инит пайчик

--- finsite/serializer/__init__.py
+++ finsite/serializer/__init__.py
@@ -1,2 +1,26 @@
-from .category import CategorySerializer
-from .products import ProductSerializer
+from .category import *
+from .products import *
+
+
+
+
+# Динамический импорт, но при его использовании не работает дополнение кода:
+
+# import os
+# import glob
+# import importlib
+#
+# package_dir = os.path.dirname(__file__)
+#
+# module_paths = glob.glob(os.path.join(package_dir, "*.py"))
+#
+# module_paths = [path for path in module_paths if not path.endswith('__init__.py')]
+#
+# for module_path in module_paths:
+#     module_name = os.path.basename(module_path)[:-3]
+#     module = importlib.import_module(f'.{module_name}', package=__name__)
+#
+#     for attr_name in dir(module):
+#         attr = getattr(module, attr_name)
+#         if isinstance(attr, type):
+#             globals()[attr_name] = attr
