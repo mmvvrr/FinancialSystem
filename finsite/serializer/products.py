c13bd5ec4218ff246f62ecbe713af67a8d528166
CyberKolyn <epictale01@mail.ru>
2024-05-18 23:32:55 +0900

    add price product

--- finsite/serializer/products.py
+++ finsite/serializer/products.py
@@ -3,11 +3,15 @@ from rest_framework import serializers
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    # category = serializers.SerializerMethodField()
+
+    prices = serializers.SerializerMethodField()
+
+    def get_prices(self, obj):
+        return obj.prices.first().price
 
     class Meta:
         model = Product
-        fields = ['pk', 'url', 'name', 'description']
+        fields = ['pk', 'url', 'name', 'description', 'prices']
 
 # class ProductSerializer(serializers.HyperlinkedModelSerializer):
 #     # category = serializers.SerializerMethodField()
--- finsite/view/product.py
+++ finsite/view/product.py
@@ -2,9 +2,17 @@ from rest_framework import permissions, viewsets
 from finsite.serializer import ProductSerializer
 from finsite.models import *
 import django_filters.rest_framework
+from django.db.models import Prefetch
+
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related(
+        Prefetch(
+            'prices',
+            queryset=ProductPriceHistory.objects.order_by('-created_at')
+        )
+    )
+
     serializer_class = ProductSerializer
     #permission_classes = [permissions.IsAuthenticated]
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
