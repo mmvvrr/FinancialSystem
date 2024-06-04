8cefafdc726289e551dac9906eb7b17324f605f8
Moskaclvhuk VR <mmvvrr1@gmail.com>
2024-06-04 11:59:19 +0900

    new link

--- financial_system_app/src/app/login/page.js
+++ financial_system_app/src/app/login/page.js
@@ -44,7 +44,7 @@ const Home = function () {
       <div className="flex align-items-center justify-content-center">
     <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
         <div className="text-center mb-5">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwMpI5mMM0wba_t7-FqOQYbiodZX1OqRoNw&s" alt="hyper" height={50} className="mb-3" />
+            <img src="https://aggregate.digital/v3_images/features/i_charting.svg" alt="hyper" height={50} className="mb-3" />
             <div className="text-900 text-3xl font-medium mb-3">Добро пожаловать в систему аналитики!</div>
         </div>
 
@@ -54,7 +54,7 @@ const Home = function () {
 
             <label htmlFor="password" className="block text-900 font-medium mb-2">Пароль</label>
             <InputText value={password} onChange={(e) => setPassword(e.target.value)}  id="password" type="password" placeholder="Пароль" className="w-full mb-3" />
-            <Button label="Войти" icon="pi pi-user" className="w-full" />
+            <Button label="Войти" icon="pi pi-user" className="w-full" onClick={clickLogin}/>
         </div>
     </div>
 </div>
