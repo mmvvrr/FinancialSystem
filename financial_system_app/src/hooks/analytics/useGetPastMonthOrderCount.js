968b6315c6d8716765f9a4e7d1f60df7093070e1
CyberKolyn <epictale01@mail.ru>
2024-06-07 18:47:45 +0900

    minor fix

--- financial_system_app/src/hooks/analytics/useGetPastMonthOrderCount.js
+++ financial_system_app/src/hooks/analytics/useGetPastMonthOrderCount.js
@@ -5,7 +5,7 @@ const useGetPastMonthOrderCount = function (orderCountsPartitionByMonth) {
 
   const analyticOrderByMonth = orderCountsPartitionByMonth.find(order => Number(order.month) === currentMonth);
 
-  return analyticOrderByMonth?.count.toLocaleString('ru-RU') || "Нет данных";
+  return analyticOrderByMonth?.count.toLocaleString('ru-RU') || orderCountsPartitionByMonth[orderCountsPartitionByMonth.length - 1].count.toLocaleString('ru-RU');
 }
 
 export default useGetPastMonthOrderCount;
